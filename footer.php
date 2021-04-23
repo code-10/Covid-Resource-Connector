a4da0a374a9aa46d2296fec4bbf67e02bdfafe86
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-23 20:52:06 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+<!-- Footer -->
+<footer class="page-footer font-small special-color-dark pt-4">
+
+  <!-- Footer Elements -->
+  <div class="container">
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">©<!--© 2020 Copyright:-->
+    <a href="#">covid resource connector</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+    
+    
+</html>
