42ce71e6581eaf47bc8b260bc7ba971e09929d77
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-24 02:51:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
          </div>
          <div class="navbar-nav ml-auto">
             <?php if(isset($_SESSION['user_name'])) {
-               echo '<a href="profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
+               echo '<a href="pages/profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['user_name'].'</i></a>';
                echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
                }
                else{
