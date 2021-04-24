13bf182eabae77239d4ab7e345a30053d5a4edbe
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-25 01:20:58 +0530

    Update requests.php

--- pages/requests.php
+++ pages/requests.php
@@ -156,6 +156,15 @@
   								Add a Comment
 							</button>
 
+							
+							
+							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+  								View Comments
+							</button>
+							
+							
+							
+							
 						<!-- Modal -->
 						<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   							<div class="modal-dialog modal-dialog-centered" role="document">
@@ -181,6 +190,37 @@
     							</div>
   							</div>
 						</div>
+						<!--add comment-->
+							
+							
+							
+						<!--view comments-->
+						<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  							<div class="modal-dialog" role="document">
+    								<div class="modal-content">
+      									<div class="modal-header">
+        									<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          										<span aria-hidden="true">&times;</span>
+        									</button>
+      									</div>
+      									<div class="modal-body">
+        									will show posts in a min
+      									</div>
+      									<div class="modal-footer">
+        									<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      									</div>
+    								</div>
+  							</div>
+						</div>			
+				
+						<!--view comments end-->
+							
+							
+							
+							
+					
+							
 							
 							
 							
