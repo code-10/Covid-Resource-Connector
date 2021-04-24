678e6a76d2947c64a1dc014574e3f57dcdbb3a22
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-24 22:35:27 +0530

    Update verify_create_request.php

--- pages/verify_create_request.php
+++ pages/verify_create_request.php
@@ -21,12 +21,12 @@
     $description = $_POST['description'];
     $phone_number = $_POST['phonenumber'];
 
-    $con->query("insert into post(email,state,city,description,ph_no) values('".mysqli_real_escape_string($con,$email)."','".mysqli_real_escape_string($con,$state)."','".mysqli_real_escape_string($con,$city)."','".mysqli_real_escape_string($con,$description)."','$phone_number')");
+    $res = $con->query("insert into post(email,state,city,description,ph_no) values('".mysqli_real_escape_string($con,$email)."','".mysqli_real_escape_string($con,$state)."','".mysqli_real_escape_string($con,$city)."','".mysqli_real_escape_string($con,$description)."',$phone_number)");
       
-    var_dump($con->error);   
+    var_dump($res);   
        
-    header("Location:requests.php");
-    die();
+    //header("Location:requests.php");
+    //die();
 
     }
 ?>
