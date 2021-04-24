9bd3d3736d3f6c4a2fbaad254ef7aa6fd773cdf4
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-25 01:47:19 +0530

    Update verify_comment.php

--- pages/verify_comment.php
+++ pages/verify_comment.php
@@ -1,1 +1,24 @@
-<?php ?>
+<?php include_once '../header.php'; session_start(); ?>
+<?php include_once '../libraries/shield.php'; ?>
+<?php
+
+   $visit = $_SERVER['REQUEST_URI'];
+  	$visit = substr($visit,1);
+
+  	$_SESSION['visit'] = $visit;		
+
+?>
+
+<?php
+
+    $con = getCon();
+        
+    if(isset($_POST['add_comment'])){
+     
+        $comment = $_POST['comment'];
+        $email = $_POST['email'];
+        $post_id = $_POST['post_id'];
+            
+    }
+
+?>
