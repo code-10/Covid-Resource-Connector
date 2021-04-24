9ea786dfaeceb251354c4aad92e6f2b05d94d1f6
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-24 18:29:23 +0530

    Create verify_create_request.php

---
+++ pages/verify_create_request.php
@@ -0,0 +1,29 @@
+<?php include_once '../header.php'; session_start(); ?>
+<?php include_once '../libraries/shield.php'; ?>
+<?php
+
+   $visit = $_SERVER['REQUEST_URI'];
+  	$visit = substr($visit,1);
+
+  	$_SESSION['visit'] = $visit;		
+
+?>
+
+<?php
+
+    $con = getCon();
+
+    if(isset($_SESSION[''])){
+
+    $email = $_POST['email'];
+    $state = $_POST['state'];
+    $city = $_POST['city'];
+    $description = $_POST['description'];
+    $phone_number = $_POST['phonenumber'];
+
+    $con->query("insert into post(email,state,city,description,ph_no) values('".mysqli_real_escape_string($con,$email)."','".mysqli_real_escape_string($con,$state)."','".mysqli_real_escape_string($con,$city)."','".mysqli_real_escape_string($con,$description)."','".mysqli_real_escape_string($con,$phone_number)."')");
+
+    header("Location:request.php");
+
+    }
+?>
