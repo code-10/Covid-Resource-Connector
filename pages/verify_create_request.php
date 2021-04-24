8b05679c9a56a8fe8e4dee22de5ed35ce41c7fb7
EagleCoder404 <harsha.jediknight@gmail.com>
2021-04-24 22:52:46 +0530

    Changed phone_number in sql to string form

--- pages/verify_create_request.php
+++ pages/verify_create_request.php
@@ -21,7 +21,7 @@
     $description = $_POST['description'];
     $phone_number = $_POST['phonenumber'];
 
-    $res = $con->query("insert into post(email,state,city,description,ph_no) values('".mysqli_real_escape_string($con,$email)."','".mysqli_real_escape_string($con,$state)."','".mysqli_real_escape_string($con,$city)."','".mysqli_real_escape_string($con,$description)."',$phone_number)"); 
+    $res = $con->query("insert into post(email,state,city,description,ph_no) values('".mysqli_real_escape_string($con,$email)."','".mysqli_real_escape_string($con,$state)."','".mysqli_real_escape_string($con,$city)."','".mysqli_real_escape_string($con,$description)."','$phone_number')"); 
        
     header("Location:requests.php");
     die();
