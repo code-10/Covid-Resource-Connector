0d70b539aaa7439eb896e22a5373d4f4c0e9cc26
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-24 18:32:22 +0530

    Create requests.php

---
+++ pages/requests.php
@@ -0,0 +1,45 @@
+<?php include_once '../header.php'; session_start(); ?>
+<?php include_once '../libraries/shield.php'; ?>
+<?php
+
+   $visit = $_SERVER['REQUEST_URI'];
+  	$visit = substr($visit,1);
+
+  	$_SESSION['visit'] = $visit;
+
+?>
+
+<body>
+   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a href="../index.php" class="navbar-brand">CRC</a>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+         <div class="navbar-nav">
+            <a href="../index.php" class="nav-item nav-link">Home</a>
+            <a href="#" class="nav-item nav-link active">About</a>
+         </div>
+         <div class="navbar-nav ml-auto">
+            <?php if(isset($_SESSION['email'])) {
+               echo '<a href="profile.php" class="nav-item nav-link active"><i class="fa fa-user-o">  '.$_SESSION['email'].'</i></a>';
+               echo '<a href="../sign_in/logout.php" class="nav-item nav-link">Logout</a>';
+               }
+               else{
+               echo '<a href="../sign_in/google_sign_in.php" class="nav-item nav-link">Sign in</a>';
+               }
+               ?>
+         </div>
+      </div>
+   </nav>
+  
+  
+  
+  
+  
+  <h2>displaying requests in a min</h2>
+  
+  
+  
+</body>
+  
