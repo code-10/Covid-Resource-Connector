36228aaf6b40eccd9e523ad04e2ddbfb76060bc3
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-25 14:58:23 +0530

    Update requests.php

--- pages/requests.php
+++ pages/requests.php
@@ -266,11 +266,11 @@
 <style>
     /*Media Queries*/
 	@media (min-width:320px)  { .responsive-md{font-size:8px;}  /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  { .responsive-md{font-size:16px;}  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
-    	@media (min-width:641px)  { .responsive-md{font-size:16px;}  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
-	@media (min-width:961px)  { .responsive-md{font-size:16px;}  /* tablet, landscape iPad, lo-res laptops ands desktops */ }
-	@media (min-width:1025px) { .responsive-md{font-size:16px;}  /* big landscape tablets, laptops, and desktops */ }
-	@media (min-width:1281px) { .responsive-md{font-size:16px;}  /* hi-res laptops and desktops */ }
+	@media (min-width:481px)  { .responsive-md{font-size:12px;}  /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+    	@media (min-width:641px)  { .responsive-md{font-size:12px;}  /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:961px)  { .responsive-md{font-size:12px;}  /* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) { .responsive-md{font-size:12px;}  /* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) { .responsive-md{font-size:12px;}  /* hi-res laptops and desktops */ }
 	
 </style>
 
