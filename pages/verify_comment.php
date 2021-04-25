669a28ea97ccf7df6c41f5faac9af4d542651aa4
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-25 16:17:30 +0530

    Update verify_comment.php

--- pages/verify_comment.php
+++ pages/verify_comment.php
@@ -19,12 +19,10 @@
         $email = $_SESSION['email'];
         $post_id = $_POST['post_id'];
       
-         echo $post_id;
+        $res = $con->query("insert into comment(post_id,comment,email) values('".mysqli_real_escape_string($con,$post_id)."','".mysqli_real_escape_string($con,$comment)."','".mysqli_real_escape_string($con,$email)."')"); 
        
-        //$res = $con->query("insert into comment(post_id,comment,email) values('".mysqli_real_escape_string($con,$post_id)."','".mysqli_real_escape_string($con,$comment)."','".mysqli_real_escape_string($con,$email)."')"); 
-       
-        //header("Location:requests.php");
-        //die();
+        header("Location:requests.php");
+        die();
          
     }
 
