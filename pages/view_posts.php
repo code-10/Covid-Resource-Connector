d9876e1824de48295d8fc0cd5e6bca295c6d5c20
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-27 01:54:27 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -142,7 +142,7 @@
 	<?php } ?>
 	
    <div class="row m-4 d-flex justify-content-center">
-			<?php for($i=0;$i<$ce;$i++) { ?>
+			<?php for($i=0;$i<$ce;$i++) { echo $fix; ?>
 				<div class="col-12 col-sm-4 m-2">
 					<div class="card">
 						
