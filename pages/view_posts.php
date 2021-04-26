1ab2428ef7e64ded410e4146675dade35f348a9d
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-26 22:59:54 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -101,7 +101,7 @@
       $downvotes_e = Array();
       $time_e = Array();
 	
-      $e_posts_res = $con->query("select p.time,p.upvotes,p.downvotes,p.post_id,p.description,p.state,p.city,p.time,p.ph_no,p.email,u.first_name,u.last_name from post as p,user as u where u.email=p.email and p.email!='$email' and p.request_resource='$request_resource' order by time asc, upvotes desc, downvotes asc");
+      $e_posts_res = $con->query("select p.time,p.upvotes,p.downvotes,p.post_id,p.description,p.state,p.city,p.time,p.ph_no,p.email,p.first_name,p.last_name from post as p,user as u where u.email=p.email and p.email!='$email' and p.request_resource='$request_resource' order by time asc, upvotes desc, downvotes asc");
    
       while($e_posts_ele = $e_posts_res->fetch_assoc())
       {
