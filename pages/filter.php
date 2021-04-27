9c03ab38e6e5677674a9f1ffcd06e87e10dccabd
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 00:29:58 +0530

    Create filter.php

---
+++ pages/filter.php
@@ -0,0 +1,12 @@
+<?php
+
+  $state = $_GET['state'];
+  $city = $_GET['city'];
+  $need = $_GET['need'];
+
+  echo $state;echo "<br>";
+  echo $city;echo "<br>";  
+  echo $need;echo "<br>";
+
+?>
+
