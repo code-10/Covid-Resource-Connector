9fac227f77068d3ee620c0e440b9ea9295ae3083
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 01:43:17 +0530

    Create filter_config.php

---
+++ pages/filter_config.php
@@ -0,0 +1,17 @@
+<?php
+
+  include_once '../libraries/shield.php';
+  
+  $con = getCon();
+
+  if(isset($_POST['sid'])) {
+		
+    $city = Array();
+    $city_res = $con->query("select * from city where state_id='$_POST['sid']'"); 
+    while($city_ele = $city_res->fetch_assoc())
+        $city[] = $city_ele['city_name'];
+    
+		echo json_encode($city);
+	}
+
+?>
