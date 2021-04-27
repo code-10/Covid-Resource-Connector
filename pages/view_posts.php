4c223b07f0fef1dfb4bd9415d8303d10f7e74454
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 00:22:47 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -26,45 +26,6 @@
 	
 	
 	
-	<div class="container">
-	<form class="row row-cols-lg-auto g-3 align-items-center">
-  		<div class="col-12 col-sm-3">
-    		<label class="visually-hidden" for="inlineFormSelectPref">State</label>
-    		<select class="form-select" id="inlineFormSelectPref">
-      			<option value="1" selected>One</option>
-      			<option value="2">Two</option>
-      			<option value="3">Three</option>
-    		</select>
-  		</div>
-
-		<div class="col-12 col-sm-3">
-    		<label class="visually-hidden" for="inlineFormSelectPref">City</label>
-    		<select class="form-select" id="inlineFormSelectPref">
-      			<option value="1" selected>One</option>
-      			<option value="2">Two</option>
-      			<option value="3">Three</option>
-    		</select>
-  		</div>
-		
-		<div class="col-12 col-sm-3">
-    		<label class="visually-hidden" for="inlineFormSelectPref">Type</label>
-    		<select class="form-select" id="inlineFormSelectPref">
-      			<option value="1" selected>One</option>
-      			<option value="2">Two</option>
-      			<option value="3">Three</option>
-    		</select>
-  		</div>
-
- 		<div class="col-12">
-    		<button type="submit" class="btn btn-primary">Submit</button>
-  		</div>
-	</form>
-	</div>
-	
-	
-	
-	
-	
 	
 	
 	
