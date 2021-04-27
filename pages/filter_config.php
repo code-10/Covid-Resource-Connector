ed1879b4839947696a03b4c0bc5fa03cf2293d76
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 02:34:25 +0530

    Update filter_config.php

--- pages/filter_config.php
+++ pages/filter_config.php
@@ -8,7 +8,7 @@
 
     $sid = $_POST['sid'];
 	  
-    $city = array();
+    $city = Array();
     $city_res = $con->query("select * from city where state_id='$sid'"); 
     while($city_ele = $city_res->fetch_assoc()){
         $city[] = $city_ele['city_name'];
