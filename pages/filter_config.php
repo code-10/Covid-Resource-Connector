f64b566e608853a3f79b947a6ba733a0046399d9
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 01:59:57 +0530

    Update filter_config.php

--- pages/filter_config.php
+++ pages/filter_config.php
@@ -10,8 +10,9 @@
 	  
     $city = Array();
     $city_res = $con->query("select * from city where state_id='$sid'"); 
-    while($city_ele = $city_res->fetch_assoc())
+    while($city_ele = $city_res->fetch_assoc()){
         $city[] = $city_ele['city_name'];
+    }
     
 		echo json_encode($city);
 	}
