e46391902f14ac231fa2b36c4faf56d56cec2a7f
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 11:59:38 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -75,7 +75,7 @@
 		<div class="col-12 col-sm-3 text-center">
 			<label for="inputuser">Need</label>
     			<select class="form-control" id="need" name="need" onchange='this.form.submit()'>
-				<option value="1" selected>All</option>
+				<option value="0" selected>All</option>
 				<?php for($i=0;$i<$tt;$i++) { ?>
 					<option value="<?=$tag_id[$i]?>"><?=$tag_name[$i]?></option>
 				<?php } ?>
