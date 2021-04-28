b4e0ba5fc9ecdccaff2382006f1f99042d141fc5
Manoj Kumar <manojkumar333619@gmail.com>
2021-04-28 11:58:58 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -57,6 +57,7 @@
   		<div class="col-12 col-sm-3 text-center">
 			<label for="inputuser">State</label>
     			<select class="form-control" id="state" name="state" required>
+				<option selected disabled>select need</option>
 				<option value="All" selected>All</option>
 				<?php for($i=0;$i<$cc;$i++) { ?>
 					<option value="<?=$state_name[$i]?>"><?=$state_name[$i]?></option>
