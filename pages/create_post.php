c979bdee76db31a7c4c3bad88a5e78bff1d92499
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-03 16:10:22 +0530

    Update create_post.php

--- pages/create_post.php
+++ pages/create_post.php
@@ -82,7 +82,7 @@
    					<div class="row d-flex justify-content-center">
 							<div class="col-lg-12">
 								
-								<form id="login-form" method="POST" action="verify_post.php" style="display: block;">
+								<form id="login-form" method="POST" action="verify_post.php" style="display: block;" onsubmit="document.getElementById('createdisable').disabled=true;>
 								<div class="form-group">
 					          <label for="inputuser">First Name</label>
 					            <input type="text" class="form-control" id="inputfirst_name" placeholder="firstname" name="first_name" required>
@@ -172,12 +172,18 @@
 	
 <script>
     
-	document.getElementById('createdisable').onclick = function() {
+	/*document.getElementById('createdisable').onclick = function() {
         	var btn = document.getElementById('createdisable');
 		btn.disabled = true;
 		//btn.style.padding = "0.5em 1em";
         	btn.innerText = 'Saving....';
     }
+	
+	$(document).ready(function() {
+    		$('form').submit(function() {
+        		$(this).find(':button[type=submit]').prop('disabled', true);
+    		});
+	});*/
 </script>
 	
 	
