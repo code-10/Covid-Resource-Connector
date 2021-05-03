2187ac3a9b4b62b788a3d2a30301414a3adc884b
EagleCoder404 <harsha.jediknight@gmail.com>
2021-05-03 16:33:33 +0530

    fuck yeah

--- pages/verify_post.php
+++ pages/verify_post.php
@@ -13,7 +13,6 @@ authentication_required();
 
     $con = getCon();
 
-    if(isset($_POST['create_post'])){
 
         $email =  mysqli_real_escape_string($con,$_POST['email']);
         $state = mysqli_real_escape_string($con,$_POST['state']);
@@ -33,7 +32,6 @@ authentication_required();
             $res = $con->query("update post set state='$state',tag_id='$tag_id',city='$city',description='$description',ph_no='$phone_number',request_resource='$request_resource' where post_id='$post_id'"); 
         }
         $type = $request_resource === "0" ? "type=request" : "type=resource";
-        header("Location:view_posts.php?$type");
+        header("Location:view_posts.php?".$type);
         die();
-    }
 ?>
