8a91a7b1e47a189086fe79ca1a89325a1cac3057
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-04 16:52:51 +0530

    Update verify_post.php

--- pages/verify_post.php
+++ pages/verify_post.php
@@ -29,30 +29,39 @@ authentication_required();
       
 
         if(!isset($_POST['modify'])){
+            
             $res = $con->query("insert into post(email,state,city,description,ph_no,request_resource,tag_id,first_name,last_name) values('$email','$state','$city','$description','$phone_number','$request_resource','$tag_id','$first_name','$last_name')");    
-           
-             $latest_id = $con->insert_id;
-           
-            $needs = Array();      
-            if(!empty($_POST['needs'])){
-               foreach($_POST['needs'] as $n){
-                  $needs[] = $n;
-               }
-            }
-           
-            echo $latest_id;echo "<br>";
-            print_r($needs);
-           
-            $nc = count($needs);
-            for($i=0;$i<$nc;$i++)
-            {
-               $con->query("insert into needs(post_id,tag_id) values('$latest_id','$needs[$i]')");
-            }
+        
         }
         else
         {
             $res = $con->query("update post set state='$state',tag_id='$tag_id',city='$city',description='$description',ph_no='$phone_number',request_resource='$request_resource' where post_id='$post_id'"); 
+           
+            $con->query("delete from needs where post_id='$post_id'");
+           
         }
+      
+        //needs
+        $needs = Array();      
+        if(!empty($_POST['needs'])){
+         foreach($_POST['needs'] as $n){
+           $needs[] = $n;
+         }
+        }
+           
+        $nc = count($needs);
+        for($i=0;$i<$nc;$i++)
+        {
+            if(!isset($_POST['modify'])){
+                $con->query("insert into needs(post_id,tag_id) values('$latest_id','$needs[$i]')");
+            }
+            else
+            {
+                $con->query("insert into needs(post_id,tag_id) values('$post_id','$needs[$i]')");
+            }
+        }
+
+
         $type = $request_resource === "0" ? "type=request" : "type=resource";
         header("Location:view_posts.php?".$type);
         die();
