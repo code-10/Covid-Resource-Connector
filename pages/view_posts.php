820995dbdca6ec4566beb15c89090292f39623cc
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-05 01:44:43 +0530

    Update view_posts.php

--- pages/view_posts.php
+++ pages/view_posts.php
@@ -91,7 +91,7 @@
     			</select>
 		</div>		
 		
-		<div class="col-12 col-sm-3 text-center p-4"><button class="btn btn-primary" name="filter" type="submit">filter</button></div>
+		<div class="col-12 col-sm-2 text-center p-4"><button class="btn btn-primary" name="filter" type="submit">filter</button></div>
 		
 	</form>
 	</div>
