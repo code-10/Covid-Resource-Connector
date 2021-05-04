ca9965eeb99687fda832be9a8dbbfe3b76927961
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-04 19:20:26 +0530

    Update create_post.php

--- pages/create_post.php
+++ pages/create_post.php
@@ -119,11 +119,11 @@
 									
 						<div class="form-group">
 								<label for="inputuser">Need</label>
-    					<select class="form-control" id="tag" name="tag_id">
+    					<!--<select class="form-control" id="tag" name="tag_id">
                               				<?php for($k=0;$k<$tag_count;$k++) { ?>
 					      			<option value="<?=$tag_id[$k]?>"><?=$tag_name[$k]?></option>
 							<?php } ?>
-    					</select>
+    					</select>-->
 									</div>		
 									
 									
