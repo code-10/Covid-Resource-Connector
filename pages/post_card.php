b930493ee533b32722ebf981b58b89678c64b39c
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-04 19:14:55 +0530

    Update post_card.php

--- pages/post_card.php
+++ pages/post_card.php
@@ -91,6 +91,7 @@ function renderUserPost($data, $type, $user_email=NULL){
         <h5 class="card-header p-3">
         $first_name&nbsp<?=$last_name?>&nbsp
         $postOptions
+        <span class="badge badge-pill badge-info m-1">$tag_name</span>
         $needs_display
         </h5>
         <div class="card-body p-3">
