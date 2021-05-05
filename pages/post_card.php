02912fbc4225e1acbc1babbd0bf072edeb938cda
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-06 00:38:55 +0530

    Update post_card.php

--- pages/post_card.php
+++ pages/post_card.php
@@ -2,6 +2,7 @@
 include_once "../libraries/shield.php";
 include_once "../header.php";
 session_start();
+$cc=0;
 date_default_timezone_set('Asia/Kolkata');
 function getUserOptions($post_id){
     $html=<<<EOD
@@ -88,13 +89,13 @@ function renderUserPost($data, $type, $user_email=NULL){
     $html=<<<START
     <div class="col-12 col-sm-4 m-2">
     <div class="card">
-        <h5 class="card-header p-3" type="button" data-toggle="collapse" data-target="#collapse_m<?=$cc?>" aria-expanded="false" aria-controls="collapseExample">
+        <h5 class="card-header p-3" type="button" data-toggle="collapse" data-target="#collapse_m$cc" aria-expanded="false" aria-controls="collapseExample">
         $first_name&nbsp<?=$last_name?>&nbsp
         $postOptions
         <!--<span class="badge badge-pill badge-info m-1">$tag_name</span>-->
         $needs_display
         </h5>
-        <div class="collapse m-2" id="collapse_m<?=$cc?>">
+        <div class="collapse m-2" id="collapse_m$cc?">
             
         <div class="card-body p-3">
             <h5 class="card-title">$city, $state</h5>
