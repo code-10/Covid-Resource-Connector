fcac9fd3d726d2dd777fe3c04c21e004cdd36c4e
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-05 21:20:51 +0530

    Update delete_comment.php

--- pages/delete_comment.php
+++ pages/delete_comment.php
@@ -2,9 +2,16 @@
  include_once '../libraries/shield.php';
 $con = getCon();
 
+$email = $_SESSION['email'];
+
 if(isset($_SESSION['email']))
 {
     $comment_id = $_GET['comment_id'];
-    $con->query("delete from comment where comment_id='$comment_id'");
-}
+    //$con->query("delete from comment where comment_id='$comment_id'");
+ 
+    if($email === 'groot@gmail.com')
+            $con->query("delete from comment where comment_id='$comment_id'"); // if admin, ignore email
+        else
+            $con->query("delete from comment where comment_id='$comment_id' and email='$email'"); // if not admin, check if owner of post is deleting
+    }
 ?>
