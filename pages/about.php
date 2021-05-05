804ac7219dbfa6a7ed20e0b6244c176de511334e
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-06 02:08:06 +0530

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -10,5 +10,12 @@
 
 <body>
    <?php include_once "../navBar.php"; ?>
+   
+   
+   <div class="text-center">
+      <h4 class="m-2">Welcome to Covid Resource Connector, A Non-profit Website.</h4>
+      <p class="m-2">Our Website connects the communication gap between people who want to help and people who want help regarding covid related queries.</p>
+      <p class="m-2">We cannot guarantee the availability nor the validity of the post. Please be advised, verify before taking action.</p>
+   </div>
 </body>
   
