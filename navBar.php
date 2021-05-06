77b1922dacc08fbe2eb448960be571c1c5dfb361
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-06 16:33:53 +0530

    Update navBar.php

--- navBar.php
+++ navBar.php
@@ -1,5 +1,12 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-      <a href="../index.php" class="navbar-brand">CRC</a>
+      <a href="../index.php" class="navbar-brand">
+            <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" viewBox="0 0 46 46">
+                  <g id="Group_1" data-name="Group 1" transform="translate(-1858 -1187)">
+                  <circle id="Ellipse_3" data-name="Ellipse 3" cx="23" cy="23" r="23" transform="translate(1858 1187)" fill="#006e5f"/>
+                  <text id="CRC" transform="translate(1881 1216)" fill="#fff" font-size="16" font-family="Poppins-Bold, Poppins" font-weight="700"><tspan x="-17.2" y="0">CRC</tspan></text>
+                  </g>
+            </svg>
+      </a>
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
       <span class="navbar-toggler-icon"></span>
       </button>
