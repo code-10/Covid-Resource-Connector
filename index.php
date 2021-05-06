cff6e6595f12ac19171dd5841a88ed989976fee1
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-06 15:09:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 	
 		<div class="text-center m-2">
 			<!--<h1 class="mb-5 responsive-lg">Find Resources to Fight Covid</h1>-->
-			<h4 class="m-4">Welcome to Covid Resource Connector, A Non-profit Organization.</h4>
-      			<p class="m-2">Our Website bridges the communication gap between people who want to help and people who want help regarding covid related queries.</p>
+			<h4 class="m-4 responsive-md">Welcome to Covid Resource Connector, A Non-profit Organization.</h4>
+      			<p class="m-2 responsive-tx">Our Website bridges the communication gap between people who want to help and people who want help regarding covid related queries.</p>
 			<br><br>
 			<h2 class="responsive-md mb-4">Find resources <a class="btn btn-info btn-sm" href="pages/view_posts.php?type=resource" role="button">View All Available Resources</a></h2>
 			<h2 class="responsive-md mb-4">Want to Help? <a class="btn btn-success btn-sm" href="pages/view_posts.php?type=request" role="button">View Requests & Respond</a></h2>
@@ -40,11 +40,11 @@
 
 <style>
     /*Media Queries*/
-	@media (min-width:320px)  { .responsive-md{font-size:24px;} .responsive-lg{font-size:32px;} /* smartphones, iPhone, portrait 480x320 phones */ }
-	@media (min-width:481px)  { .responsive-md{font-size:32px;} .responsive-lg{font-size:64px;} /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
-    	@media (min-width:641px)  { .responsive-md{font-size:32px;} .responsive-lg{font-size:64px;}/* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
-	@media (min-width:961px)  { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;}/* tablet, landscape iPad, lo-res laptops ands desktops */ }
-	@media (min-width:1025px) { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;}/* big landscape tablets, laptops, and desktops */ }
-	@media (min-width:1281px) { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;}/* hi-res laptops and desktops */ }
+	@media (min-width:320px)  { .responsive-md{font-size:24px;} .responsive-lg{font-size:32px;} .responsive-tx{font-size:16px;} /* smartphones, iPhone, portrait 480x320 phones */ }
+	@media (min-width:481px)  { .responsive-md{font-size:32px;} .responsive-lg{font-size:64px;} .responsive-tx{font-size:24px;} /* portrait e-readers (Nook/Kindle), smaller tablets @ 600 or @ 640 wide. */ }
+    	@media (min-width:641px)  { .responsive-md{font-size:32px;} .responsive-lg{font-size:64px;}  .responsive-tx{font-size:24px;} /* portrait tablets, portrait iPad, landscape e-readers, landscape 800x480 or 854x480 phones */ }
+	@media (min-width:961px)  { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;} .responsive-tx{font-size:24px;}/* tablet, landscape iPad, lo-res laptops ands desktops */ }
+	@media (min-width:1025px) { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;} .responsive-tx{font-size:24px;}/* big landscape tablets, laptops, and desktops */ }
+	@media (min-width:1281px) { .responsive-md{font-size:32px;}  .responsive-lg{font-size:64px;} .responsive-tx{font-size:24px;}/* hi-res laptops and desktops */ }
 	
 </style>
