0a43c690edd034c99c78963c69514ffd3fbf346b
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-06 15:00:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,7 @@
 		<div class="text-center m-2">
 			<!--<h1 class="mb-5 responsive-lg">Find Resources to Fight Covid</h1>-->
 			<h4 class="m-4">Welcome to Covid Resource Connector, A Non-profit Organization.</h4>
-      			<p class="m-2">Our Website connects the communication gap between people who want to help and people who want help regarding covid related queries.</p>
-      			<p class="m-2">We cannot guarantee the availability nor the validity of the post. Please be advised, verify the post before taking action.</p>
+      			<p class="m-2">Our Website bridges the communication gap between people who want to help and people who want help regarding covid related queries.</p>
 			<br><br>
 			<h2 class="responsive-md mb-4">Find resources <a class="btn btn-info btn-sm" href="pages/view_posts.php?type=resource" role="button">View All Available Resources</a></h2>
 			<h2 class="responsive-md mb-4">Want to Help? <a class="btn btn-success btn-sm" href="pages/view_posts.php?type=request" role="button">View Requests & Respond</a></h2>
