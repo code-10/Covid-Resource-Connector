3f10166d964ed58bf786b2461041654170fe7e38
EagleCoder404 <harsha.jediknight@gmail.com>
2021-05-06 17:09:09 +0530

    backup page

--- pages/backup_display.php
+++ pages/backup_display.php
@@ -5,8 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include_once "../header.php"; ?>
 </head>
 <body>
-    
+    <?php include_once "../navBar.php" ?>
+    <div class='text-center my-5'>
+        <h1>We Make Backups every day, and here they are.</h1>
+        <h4 class='text-muted'>With Great Power Comes Great Responsibility</h4>
+    </div>
+    <div class='container'>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Backup Name</th>
+                    <th scope="col">Backup File</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Day 1 Backup</td>
+                    <td><a href="/backups/2021-05-06_11-16-19.xlsx">Backup File</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
 </body>
 </html>
