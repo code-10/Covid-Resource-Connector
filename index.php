77c2b81324a88566efa96c81981c39faf81f9324
Manoj Kumar <manojkumar333619@gmail.com>
2021-05-08 13:53:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -41,10 +41,10 @@
 			<div class="row">
 				<!--<div class="col col-sm-6 text-center">
 					<a class="btn btn-success rounded-pill res-mar res-pad" href="pages/create_post.php?resource=selected" role="button" style="background-color:#006E5F;">Create a Resource</a>
-				</div>
+				</div>-->
 				<div class="col col-sm-6 text-center">
 					<a class="btn btn-success rounded-pill res-mar res-pad" href="pages/create_post.php?request=selected" role="button" style="background-color:#006E5F;">Create a Request</a>
-				</div>-->
+				</div>
 			</div>
 			<br>
 			<div class='row'>
